e83c9bacc5449bf7f8302ec8a2525b0974d5e0c8
TrishaPW <40293344+TrishaPW@users.noreply.github.com>
2018-06-15 12:50:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,14 +6,14 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    if (message.content === 'site') {
+    	message.channel.send('https://retro-rp.com');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
+    if (message.content === 'credits') {
+    	message.reply('Trisha');
   	}
 });
 
